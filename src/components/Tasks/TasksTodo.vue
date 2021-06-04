a6aa12cc19f8d204d03780604e4237a211628b69
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-06-04 13:55:09 +0200

    changed animation

--- src/components/Tasks/TasksTodo.vue
+++ src/components/Tasks/TasksTodo.vue
@@ -1,27 +1,29 @@
 <template>
-<transition
-  appear
-  enter-active-class="animated zoomIn"
-  leave-active-class="animated zoomOut absolute-top"
->
-  <div >
-    <ListHeader v-if="!settings.showTasksInOneList" bgColor="bg-orange-4">Todo</ListHeader>
-    <q-list  bordered separator>
-      <Task
-        v-for="(task, key) in tasksTodo"
-        :key="key"
-        :task="task"
-        :id="key"
-      />
-    </q-list>
-  </div>
-</transition>
+  <transition
+    appear
+    enter-active-class="animated fadeIn"
+    leave-active-class="animated fadeOut absolute-top"
+  >
+    <div>
+      <ListHeader v-if="!settings.showTasksInOneList" bgColor="bg-orange-4"
+        >Todo</ListHeader
+      >
+      <q-list bordered separator>
+        <Task
+          v-for="(task, key) in tasksTodo"
+          :key="key"
+          :task="task"
+          :id="key"
+        />
+      </q-list>
+    </div>
+  </transition>
 </template>
 
 <script>
 import Task from "components/Tasks/Task";
 import ListHeader from "components/Shared/ListHeader";
-import { mapGetters} from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
   components: {
@@ -30,11 +32,9 @@ export default {
   },
   props: ["tasksTodo"],
   computed: {
-    ...mapGetters('settings', ['settings'])
+    ...mapGetters("settings", ["settings"])
   }
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
